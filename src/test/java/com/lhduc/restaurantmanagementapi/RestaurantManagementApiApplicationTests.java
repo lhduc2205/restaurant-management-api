12eaf95deec5ec0187b87a9016297d0e45854745
lhduc2205 <lehuynhduc29a1@gmail.com>
2023-10-24 09:24:33 +0700

    init project

---
+++ src/test/java/com/lhduc/restaurantmanagementapi/RestaurantManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhduc.restaurantmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
